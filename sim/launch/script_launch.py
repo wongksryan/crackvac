085b9c3524d6fc7eda54c9a9e6fa9c0e06d2bc0d
Sarah Yoo <gaeunyoo3626@gmail.com>
2025-12-20 01:52:50 -0500

    write model with xacro, add ros2_controls package

--- sim/launch/script_launch.py
+++ sim/launch/script_launch.py
@@ -1,10 +1,9 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.parameter_descriptions import ParameterValue
-
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
@@ -12,11 +11,11 @@ def generate_launch_description():
     #load model
     robot_description = ParameterValue(
         Command([
-            'cat ',
+            'xacro ',
             PathJoinSubstitution([
                 FindPackageShare('sim'),
                 'urdf',
-                'robot_model.urdf'
+                'robot_model.urdf.xacro'
             ])
         ]),
         value_type=str
@@ -28,7 +27,6 @@ def generate_launch_description():
             default_value='false',
             description='Use simulation clock'
         ),
-
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -39,11 +37,21 @@ def generate_launch_description():
                 'robot_description': robot_description
             }],
         ),
-
         Node(
             package='sim',
             executable='state_publisher',
             name='state_publisher',
             output='screen'
         ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', PathJoinSubstitution([
+                FindPackageShare('sim'),
+                'rviz',
+                'robot_display.rviz'
+            ])]
+        )
     ])
