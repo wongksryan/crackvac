242e6bc8da5b024f67626db4d7c666528dcc4e4f
Sarah Yoo <gaeunyoo3626@gmail.com>
2025-12-19 21:48:25 -0500

    add robot state launcher

---
+++ sim/launch/script_launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    #load model
+    robot_description = ParameterValue(
+        Command([
+            'cat ',
+            PathJoinSubstitution([
+                FindPackageShare('sim'),
+                'urdf',
+                'robot_model.urdf'
+            ])
+        ]),
+        value_type=str
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation clock'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'robot_description': robot_description
+            }],
+        ),
+
+        Node(
+            package='sim',
+            executable='state_publisher',
+            name='state_publisher',
+            output='screen'
+        ),
+    ])
