7f24448d35a93c54d3af4952688d1802827ad3dc
Sarah Yoo <gaeunyoo3626@gmail.com>
2025-12-29 18:18:21 -0500

    launch gazebo with controller, remove wheel collision

--- sim/launch/rviz_launch.py
+++ sim/launch/rviz_launch.py
@@ -6,29 +6,28 @@ from launch_ros.parameter_descriptions import ParameterValue
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 
+# launch rviz with swerve controller
 def generate_launch_description():
     use_sim_time = True
 
-    pckg_path = FindPackageShare('sim')
-    xacro_file = PathJoinSubstitution([pckg_path,'urdf','robot_model.urdf.xacro'])
-    controllers_file = PathJoinSubstitution([pckg_path,'configs','controllers.yaml'])
-    rviz_file = PathJoinSubstitution([pckg_path,'configs','display.rviz'])
+    my_pckg_path = FindPackageShare('sim')
+    controllers_file = PathJoinSubstitution([my_pckg_path,'configs','controllers.yaml'])
+    rviz_file = PathJoinSubstitution([my_pckg_path,'configs','display.rviz'])
 
     # load robot model, get urdf via xacro
+    xacro_file = PathJoinSubstitution([my_pckg_path,'urdf','robot_model.urdf.xacro'])
     robot_description = ParameterValue(
         Command(['xacro ', xacro_file]),
         value_type=str
     )
 
-    # starts ros2 control
-    control_node = Node(
+    start_controller_manager = Node(
         package='controller_manager',
         executable='ros2_control_node',
         parameters=[controllers_file],
         output='both'
     )
-    # starts ros2 robot_state_publisher
-    robot_state_pub_node = Node(
+    start_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='both',
@@ -37,45 +36,42 @@ def generate_launch_description():
             'robot_description': robot_description
         }],
     )
-    # starts rviz view
-    rviz_node = Node(
+    launch_rviz = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='log',
         arguments=['-d', rviz_file]
     )
-    # starts ros2 joint_state_broadcaster
-    joint_state_broadcaster_spawner = Node(
+    spawn_joint_state_broadcaster = Node(
         package='controller_manager',
         executable='spawner',
         arguments=['joint_state_broadcaster']
     )
-    # starts swerve_controller
-    swerve_controller_spawner = Node(
+    spawn_swerve_controller = Node(
         package='controller_manager',
         executable='spawner',
         arguments=['swerve_controller', '--param-file', controllers_file]
     )
     # delay starting rviz after joint_state_broadcaster
-    delay_rviz = RegisterEventHandler(
+    delay_rviz_launch = RegisterEventHandler(
         event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node]
+            target_action=spawn_joint_state_broadcaster,
+            on_exit=[launch_rviz]
         )
     )
     # delay starting swerve_controller after joint_state_broadcaster
-    delay_swerve_controller_spawner = RegisterEventHandler(
+    delay_swerve_controller_spawn = RegisterEventHandler(
         event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[swerve_controller_spawner]
+            target_action=spawn_joint_state_broadcaster,
+            on_exit=[spawn_swerve_controller]
         )
     )
 
     return LaunchDescription([
-        control_node,
-        robot_state_pub_node,
-        joint_state_broadcaster_spawner,
-        delay_rviz,
-        delay_swerve_controller_spawner
+        start_controller_manager,
+        start_robot_state_publisher,
+        spawn_joint_state_broadcaster,
+        delay_rviz_launch,
+        delay_swerve_controller_spawn
     ])
